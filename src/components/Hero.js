eba070bbcb75eea5054f3d3582309ce65ade41bc
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-03-04 12:44:39 +0100

    svg, title and layout working

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,17 @@ function Hero() {
     <div className='hero' id='home'>
       <div className="hero-left">
         <h1 className="hero-title">catalina acosta rivera</h1>
-        <h2 className="hero-subtitle">web developer</h2>
+        <svg height="10vh" width="70vw" xmlns="http://www.w3.org/2000/svg">
+          <text x="5" y="50" fill="none" stroke="#57535b" font-size="45" font-family="Lato">web developer
+          <animate
+            attributeName="x"
+            begin="0s"
+            dur="10s"
+            from="0"
+            to="100%"
+            repeatCount="indefinite" />
+          </text>
+        </svg>
       </div>
       <div className="hero-right">
         <button className='download-cv'>download cv</button>
