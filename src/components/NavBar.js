12cb842bccd75ecfca2a2627ca2c1ccbe401cb0d
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-03-05 15:10:13 +0100

    download button working testing confetti

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
 import "../styles/NavBar.css"
 import { HashLink } from 'react-router-hash-link';
 import { BrowserRouter as Router } from "react-router-dom";
-import turtle from '../assets/turtle.png'
 
 function NavBar() {
   return (
     <div className="navbar">
       <Router>
         <ul className="links">
-          <li><HashLink to='#projects'>projects</HashLink></li>
+          <li><HashLink smooth to='#projects'>projects</HashLink></li>
           <li><HashLink to='#about'>about</HashLink></li>
           <li><HashLink to='#contact'>contact</HashLink></li>
         </ul>
-        <span id='logo'><HashLink to='#home'> C.</HashLink></span>
+        <div className="logo-container">
+          <HashLink to='#home' id="logo"> C.</HashLink>
+        </div>
       </Router>
     </div>
   );
---
+++ src/helpers/SubtitleHero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function SubtitleHero() {
+  return (
+    <svg height="10vh" width="70vw" xmlns="http://www.w3.org/2000/svg">
+      <text x="5" y="50" fill="none" stroke="#57535b" font-size="45" font-family="Lato">web developer
+        <animate
+          attributeName="x"
+          begin="0s"
+          dur="10s"
+          from="0"
+          to="100%"
+          repeatCount="indefinite" />
+      </text>
+    </svg>
+  )
+}
+
+export default SubtitleHero
