9ff752d99ac06e495efe6025c7d6775969243c80
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-03-06 10:58:31 +0100

    working

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import "../styles/About.css"
+import aboutImg from '../assets/img/about-pic.jpg'
+
 
 function About() {
   return (
-    <div className="about" id='about'>about</div>
+    <div className="about" id='about'>
+      <div className="img-container">
+        <img src={aboutImg} alt="Catalina" />
+      </div>
+      <div className="about-text">
+        <h1>
+          about myself
+        </h1>
+        <p>I am originally from Bogotá, Colombia, but I have been living in Europe for the past eight years. After spending three years in Barcelona, I moved to Berlin. I studied Fashion Design in Bogotá and earned my Masters in Fashion Management in Barcelona. Since then, I have worked in the Fashion industry as an Account Manager and, most recently, as a Product Manager in Berlin.</p>
+        <p>During the Covid pandemic, I found myself with more free time than expected, which sparked my interest in web development and programming. To my surprise, I discovered that not only could I program (something I had thought was not an option for me for years), but it was also fun and creative.</p>
+        <p>In the autumn of 2022, I took the leap and enrolled in a 9-week intensive web development bootcamp at Le Wagon in Berlin. Since then, I have been convinced that I want to continue working as a developer and apply all my skills to it.</p>
+        <p>In addition to web development, I enjoy practicing yoga 🧘, baking 🧁, and dancing 🪩.</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import "../styles/Hero.css"
 import SubtitleHero from '../helpers/SubtitleHero'
-import Confetti from 'react-confetti';
+
 
 function Hero() {
   const [isDownloaded, setIsDownloaded] = useState(false);
@@ -15,13 +15,17 @@ function Hero() {
       <div className="hero-left">
         <h1 className="hero-title">catalina acosta rivera</h1>
         <SubtitleHero/>
+        {/*<div className="text-container">
+          <p>Hello there 🌈! I am a full-stack developer seeking new professional opportunities in full-stack, back-end, or front-end development. I am naturally curious, detail-oriented, and passionate about finding creative solutions to problems, streamlining processes, and developing products that meet market needs. Collaborating with others is important to me as I enjoy learning from fresh perspectives.</p>
+          <p>Lately, I've been focusing on enhancing my front-end skills, including practicing JavaScript, learning React, and exploring debugging tools like Chat GPT 🤓, which has proven to be a valuable resource.</p>
+        </div>*/}
       </div>
       <div className="hero-right">
       <a href="/CV_Catalina Acosta Rivera.pdf" download="CV_Catalina Acosta Rivera.pdf">
           <button className='download-cv' onClick={handleDownloadClick}>download cv</button>
         </a>
       </div>
-      {isDownloaded && <Confetti />}
+      {isDownloaded }
     </div>
   )
 }
