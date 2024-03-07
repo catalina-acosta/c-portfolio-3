ae5b5de02562209723b4e7da36e2936caa426c98
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-03-07 12:10:24 +0100

    contact form added, working and styled

--- src/helpers/ContactForm.js
+++ src/helpers/ContactForm.js
@@ -1,38 +1,80 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import "../styles/ContactForm.css"
 import Modal from './Modal'
-import {useState} from 'react'
 
 const ContactForm = () => {
   const form = useRef();
 
   const [openModal, setOpenModal] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_lmtd4hn', 'template_0ijvi5b', form.current, 'CT5blvDack2_s7xqK')
+    if (!name || !email || !message) {
+      alert('Please fill in all fields.');
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+
+    emailjs.sendForm('service_d6yvvsu', 'template_8ebvz37', form.current, 'CT5blvDack2_s7xqK')
       .then((result) => {
           console.log(result.text);
           console.log("message sent");
+          setOpenModal(true); // Open the modal after sending the email
+          clearFormFields(); // Clear form fields after sending the email
       }, (error) => {
           console.log(error.text);
       });
-    };
+  };
+
+  const clearFormFields = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
 
   return (
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input className="openModalBtn" onClick={() => {setOpenModal(true);}}
-        type="submit" value="Send" />
-        {openModal && <Modal closeModal ={setOpenModal}/>}
-      </form>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input
+        type="text"
+        name="user_name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <label>Email</label>
+      <input
+        type="email"
+        name="user_email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <label>Message</label>
+      <textarea
+        name="message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        required
+      />
+      <input className="openModalBtn" type="submit" value="Send" />
+      {openModal && <Modal closeModal={() => setOpenModal(false)} />}
+    </form>
   );
 }
+
 export default ContactForm;
