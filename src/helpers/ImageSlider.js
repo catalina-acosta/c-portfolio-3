f12f34ff7718c95243065e7dcc8a5c98aa5d8864
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-03-22 17:13:27 +0100

    project-info styled

--- src/helpers/ImageSlider.js
+++ src/helpers/ImageSlider.js
@@ -19,39 +19,41 @@ function ImageSlider(props) {
   };
 
   return (
-    <div className="slider">
-      <div className='left-arrow' onClick={goToPrevious}>
-        ❰
-      </div>
-      <div className='right-arrow' onClick={goToNext}>
-        ❱
-      </div>
-      <div className={`slide-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-        <div className="slide">
-          <div className="project-left">
-            <div className="graphic">
-              <div className="dots-nav">
-                <div className='dots'>
-                  <span className='dot'></span>
-                  <span className='dot'></span>
-                  <span className='dot'></span>
+    <div className="slider-container">
+      <div className="slider">
+        <div className='left-arrow' onClick={goToPrevious}>
+          ❰
+        </div>
+        <div className='right-arrow' onClick={goToNext}>
+          ❱
+        </div>
+        <div className={`slide-container ${isHovered ? 'hovered' : ''}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+          <div className="slide">
+            <div className="project-left">
+              <div className="graphic">
+                <div className="dots-nav">
+                  <div className='dots'>
+                    <span className='dot'></span>
+                    <span className='dot'></span>
+                    <span className='dot'></span>
+                  </div>
                 </div>
-              </div>
-              <div className="project-img-container">
-                <img src={slides[currentIndex].image} alt="" className='project-img'/>
+                <div className="project-img-container">
+                  <img src={slides[currentIndex].image} alt="" className='project-img'/>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
       {isHovered && (
-        <div>
-          <div className='project-title'>{slides[currentIndex].title}</div>
-          <div className="description">{slides[currentIndex].description}</div>
-          <div className="tech-stack">{slides[currentIndex].techStack}</div>
-          <div className="label">{slides[currentIndex].label}</div>
-        </div>
-      )}
+        <div className='project-info'>
+          <div className='project-title'><h1>{slides[currentIndex].title}</h1></div>
+            <div className="description"><p>{slides[currentIndex].description}</p></div>
+            <div className="tech-stack"><p>{slides[currentIndex].techStack}</p></div>
+            <div className="label"><p>{slides[currentIndex].label}</p></div>
+          </div>
+        )}
     </div>
   );
 }
