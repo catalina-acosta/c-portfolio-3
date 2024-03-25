07567464d29722c9c3de39997c9080ece99323c9
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-03-25 12:46:32 +0100

    navbar is responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,30 @@
+import React, { useState } from 'react';
 import "../styles/NavBar.css"
 import { HashLink } from 'react-router-hash-link';
 import { BrowserRouter as Router } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 function NavBar() {
+
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const handleLinkClick = () => {
+    setExpandNavbar(false);
+  }
+
   return (
+
     <div className="navbar">
       <Router>
-        <ul className="links">
-          <li><HashLink smooth to='#projects'>projects</HashLink></li>
-          <li><HashLink to='#about'>about</HashLink></li>
-          <li><HashLink to='#contact'>contact</HashLink></li>
+        <div className="menu-icon" onClick={() => setExpandNavbar(!expandNavbar)}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <ul className={`links ${expandNavbar ? 'expanded' : ''}`}>
+          <li><HashLink smooth to='#projects' onClick={handleLinkClick}>projects</HashLink></li>
+          <li><HashLink to='#about' onClick={handleLinkClick}>about</HashLink></li>
+          <li><HashLink to='#contact' onClick={handleLinkClick}>contact</HashLink></li>
         </ul>
         <div className="logo-container">
           <HashLink to='#home' id="logo"> c.</HashLink>
