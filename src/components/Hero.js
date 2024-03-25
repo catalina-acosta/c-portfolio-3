3519ced2faba147a3a5b7c16092ee6db97feaeb7
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-03-25 15:35:42 +0100

    last adjustments for small devises

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,16 +15,12 @@ function Hero() {
       <div className="hero-left">
         <h1 className="hero-title">catalina acosta rivera</h1>
         <SubtitleHero/>
-        {/*<div className="text-container">
-          <p>Hello there 🌈! I am a full-stack developer seeking new professional opportunities in full-stack, back-end, or front-end development. I am naturally curious, detail-oriented, and passionate about finding creative solutions to problems, streamlining processes, and developing products that meet market needs. Collaborating with others is important to me as I enjoy learning from fresh perspectives.</p>
-          <p>Lately, I've been focusing on enhancing my front-end skills, including practicing JavaScript, learning React, and exploring debugging tools like Chat GPT 🤓, which has proven to be a valuable resource.</p>
-        </div>*/}
-      </div>
-      <div className="hero-right">
-      <a href="/CV_Catalina Acosta Rivera.pdf" download="CV_Catalina Acosta Rivera.pdf">
-          <button className='download-cv' onClick={handleDownloadClick}>download cv</button>
+        <a href="/CV_Catalina Acosta Rivera.pdf" download="CV_Catalina Acosta Rivera.pdf" className='download-cv' onClick={handleDownloadClick}>
+          download cv
         </a>
       </div>
+      <div className="hero-right">
+</div>
       {isDownloaded }
     </div>
   )
--- src/helpers/SubtitleHero.js
+++ src/helpers/SubtitleHero.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SubtitleHero() {
   return (
-    <svg height="10vh" width="70vw" xmlns="http://www.w3.org/2000/svg">
-      <text x="5" y="50" fill="none" stroke="#57535b" font-size="45" font-family="Lato">web developer
+    <svg height="10vh" width="100vw" xmlns="http://www.w3.org/2000/svg">
+      <text x="-5" y="50" fill="none" stroke="#57535b" font-size="45" font-family="Lato">web developer
         <animate
           attributeName="x"
           begin="0s"
