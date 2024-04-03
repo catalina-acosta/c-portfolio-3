28f2588d135261028473c124ab6a70209679ba1a
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2024-04-03 11:21:32 +0200

    final fixes

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -6,31 +6,30 @@ import cPorfolio2 from '../assets/img/c-acosta2.jpg';
 export const ProjectList = [
   {
     name: "jeronimo escobar design",
-    description: "website for the colombian designer and architect Jeronimo Escobar",
+    description: "Website for the colombian designer and architect Jeronimo Escobar.",
     techStack: ["React Js |", " JavaScript |", " CSS |", " HTML"],
     img: jescobarImg,
     label: "freelance work",
     link: "https://jeronimoescobar.com/",
-    git: "https://github.com/catalina-acosta/jescobar-design-react/tree/master"
   },
   {
-    name: "Find My Midwife",
-    description: "web App where mom's and midwifes find each other",
+    name: "find my midwife",
+    description: "Web app where mom's and midwifes find each other.",
     techStack: ["Ruby on Rails |", " Ruby |", " PostgreSQL |", " CSS |", " HTML"],
     img: fmmwImg,
     git: "https://github.com/catalina-acosta/find_my_midwife"
   },
   {
-    name: "personal porfolio V.2.",
-    description: "the newest version of my personal porfolio",
+    name: "personal porfolio v.2.",
+    description: "The newest version of my personal porfolio.",
     techStack: ["React Js |", " JavaScript |", " CSS |", " HTML"],
     img: cPorfolio2,
     git: "https://github.com/catalina-acosta/c-portfolio-3"
 
   },
   {
-    name: "personal porfolio V.1.",
-    description: "the first version of my personal porfolio",
+    name: "personal porfolio v.1.",
+    description: "The first version of my personal porfolio.",
     techStack: ["CSS |", " HTML"],
     img: cPorfolio1,
     git: "https://github.com/catalina-acosta/c_acosta_portfolio"
